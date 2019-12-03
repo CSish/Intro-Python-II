b93097347c69a39e90a745bb14e71458c8095606
tactnician <tactnician@gmail.com>
2019-12-03 17:51:27 -0500

    Added player object with crud and move methods

--- src/player.py
+++ src/player.py
@@ -1,2 +1,31 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player: 
+    def __init__(self, name, location):
+        self.name = name
+        self.location = location
+        self.items = []
+
+    def make_move(self, new_location):
+        self.location = new_location
+        print(f'Location: {self.location.name}')
+        print(self.location.desc)
+
+
+    #Crud Operations
+    def add_item(self, item_obj):
+        self.items.append(item_obj)
+        print(f'You picke up a/an: {item_obj}')
+        return self.items
+
+    def list_items(self):
+        return self.items
+
+    def show_item(self, item_obj): #retruns index
+        return self.items.index(item_obj)
+
+    def remove_item(self, item_obj):
+        self.items.remove(item_obj)
+        self.location.add_item(item_obj)
+        print(f'You put down a/an: {item_obj}')
--- src/room.py
+++ src/room.py
@@ -9,10 +9,10 @@ class Room:
         self.items = ['test', 'item1', 'item2', 'item3',]
 
     #Methods for changing state
-    def _add_items(self, item_obj):
+    def add_item(self, item_obj):
         self.items.append(item_obj)
         print(f"You picked up a/an: {item_obj}")
 
-    def _remove_item(self, item_obj):
+    def remove_item(self, item_obj):
         self.items.remove(item_obj)
         print(f"You put down a/an: {item_obj}")
