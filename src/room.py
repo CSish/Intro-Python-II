964e3cffe5e730425ee3c504ed0a50a4af0750b3
tactnician <tactnician@gmail.com>
2019-12-03 16:56:13 -0500

    Added Room class toroom.py with methods _add_item and _remove_item

--- src/room.py
+++ src/room.py
@@ -1,2 +1,18 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+class Room:
+    #Adjectives for holding state 
+    def __init__(self, name, desc):
+        self.name = name
+        self.desc = desc
+        self.items = ['test', 'item1', 'item2', 'item3',]
+
+    #Methods for changing state
+    def _add_items(self, item_obj):
+        self.items.append(item_obj)
+        print(f"You picked up a/an: {item_obj}")
+
+    def _remove_item(self, item_obj):
+        self.items.remove(item_obj)
+        print(f"You put down a/an: {item_obj}")
